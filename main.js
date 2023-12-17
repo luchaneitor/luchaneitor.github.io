595a59c021f10d91d6b97dfc91f7b38933740a68
luchaneitor <154029026+luchaneitor@users.noreply.github.com>
2023-12-16 18:21:39 -0600

    oladios

---
+++ main.js
@@ -0,0 +1,16 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+var miAudio = document.getElementById('Elvis');
+
+// Función para iniciar la reproducción al hacer clic en cualquier lugar de la pantalla
+function iniciarReproduccion() {
+if (miAudio.paused) {
+        miAudio.play();
+        } else {
+                miAudio.pause();
+        }
+}
+// Agregar un evento de clic al documento que llama a la función
+document.addEventListener('click', iniciarReproduccion);
